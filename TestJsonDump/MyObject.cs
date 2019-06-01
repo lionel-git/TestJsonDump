71209ea1d5c624f1c45de2567124c3884fc58a83
Lionel Desorme <lionel.desorme@free.fr>
2019-06-01 18:47:12 +0200

    Initiaol Commit

---
+++ TestJsonDump/MyObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJsonDump
+{
+    public class MyObject
+    {
+        public int Age { get; set; }
+        public int Age2 { get; } = 5;
+
+        public List<string> Names { get; set; } = new List<string>();
+        public Dictionary<string, int> NameDatas { get; set; } = new Dictionary<string, int>();
+
+        public void InitTest()
+        {
+            Age = 1;
+            Names.Add("toto");
+            Names.Add("titi");
+
+            NameDatas.Add("toto", 1);
+            NameDatas.Add("titi", 5);
+        }
+
+    }
+}
---
+++ TestJsonDump/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJsonDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = new MyObject();
+            o.InitTest();
+            var s = JsonConvert.SerializeObject(o, Formatting.Indented);
+            Console.WriteLine(s);
+        }
+    }
+}
